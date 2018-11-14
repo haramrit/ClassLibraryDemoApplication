e70f4e63d6e8c7d71d4b94ebfa211b30890493a8
Har Amrit Pal Singh Panesar <panehaps@myumanitoba.ca>
2018-11-14 11:53:33 -0800

    Create library and client application

---
+++ CarClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+
+namespace CarClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Car Client Using ClassLibrary ****");
+            // Make a sports car.
+            SportsCar viper = new SportsCar("Viper", 240, 40);
+            viper.TurboBoost();
+
+            // Make a minivan
+            MiniVan mv = new MiniVan();
+            mv.TurboBoost();
+
+            Console.WriteLine("Done. Press any key to terminate");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassLibrary/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    //Represent the state of an engine
+    public enum EngineState
+    {
+        engineAlive, engineDead
+    }
+
+    // abstract base class
+    public abstract class Car
+    {
+        public string PetName { get; set; }
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+
+        protected EngineState egnState = EngineState.engineAlive;
+        public EngineState EngineState => egnState;
+        public abstract void TurboBoost();
+
+        public Car() { }
+        public Car(String name, int maxSp, int currSp)
+        {
+            PetName = name;
+            MaxSpeed = maxSp;
+            CurrentSpeed = currSp;
+        }
+    }
+}
